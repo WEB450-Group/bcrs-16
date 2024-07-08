1e13134b55e51841c53f2f8240015b1dfeec3573
jb830 <jbrumfield623@gmail.com>
2024-07-08 13:42:08 -0700

    Added disabled employee table and responsive employee lists

--- src/app/admin/employee-list/employee-list.component.ts
+++ src/app/admin/employee-list/employee-list.component.ts
@@ -21,18 +21,32 @@ export class EmployeeListComponent {
   employees: Employee[];
   errorMessage: '';
   isLoading: boolean;
+  isDisabled: boolean;
+  disabledEmployees: Employee[];
+  currentEmployees: Employee[]
 
   constructor(private employeeService: EmployeeService) {
     //initialize variables
     this.employees = [];
+    this.disabledEmployees = [];
     this.errorMessage = "";
     this.isLoading = true;
+    this.isDisabled = false;
+    this.currentEmployees = [];
 
     // inject employee service and subscribe to employee API
     this.employeeService.findAllEmployees().subscribe({
       //when data fetched assign employee data from database to employee property
       next: (employees: any) => {
-        this.employees = employees
+        this.employees = employees;
+        
+        //use disable() to filter employees list for isDisabled = true and create a new array of diabled employees 
+        this.currentEmployees = this.employees.filter(this.current);
+        console.log(this.currentEmployees)
+
+        //use disable() to filter employees list for isDisabled = true and create a new array of diabled employees 
+        this.disabledEmployees = this.employees.filter(this.disabled);
+        console.log(this.disabledEmployees)
       },
       // if data cannot be fetch console error message
       error: (err) => {
@@ -44,5 +58,14 @@ export class EmployeeListComponent {
         this.isLoading = false;
       }
     });
+  }
+  
+  // return true if isDisabled is true on an employee ofject in employee list 
+  current(employee: Employee): boolean {
+    return employee.isDisabled === false;
+  }
+  // return true if isDisabled is true on an employee ofject in employee list 
+  disabled(employee: Employee): boolean {
+    return employee.isDisabled === true;
   }
 }
