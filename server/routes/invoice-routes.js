5aa1fa442c13ad404dbf02578b305838ec927d36
Zadkiel <zadkiel679@gmail.com>
2024-07-16 19:01:31 -0400

    Finish the create invoice API and tested with swagger.

--- server/routes/invoice-routes.js
+++ server/routes/invoice-routes.js
@@ -25,18 +25,18 @@ const lineItemSchema = {
   items: {
     type: 'object',
     properties: {
+      itemId: { type: 'number' },
       itemName: { type: 'string' },
       price: { type: 'number' }
     },
   },
-  required: [ 'itemName' , 'price' ],
+  required: [ 'itemId', 'itemName' , 'price' ],
   additionalProperties: false
 };
 
 const invoiceSchema = {
   type: 'object',
   properties: {
-    employeeId: { type: 'number' },
     customerEmail: { type: 'string' },
     phoneNumber: { type: 'number' },
     fullName: { type: 'string' },
@@ -46,36 +46,121 @@ const invoiceSchema = {
     lineItemTotal: { type: 'number' },
     invoiceTotal: { type: 'number' }
   },
-  required: [ 'employeeId', 'customerEmail', 'phoneNumber', 'fullName', 'lineItems', 'partsAmount', 'laborAmount', 'lineItemTotal', 'invoiceTotal' ],
+  required: [ 'customerEmail', 'phoneNumber', 'fullName', 'lineItems', 'partsAmount', 'laborAmount', 'lineItemTotal', 'invoiceTotal' ],
   additionalProperties: false
 };
 
 // Routes
-router.post('/invoices/:employeeId', (req, res, next) => {
+
+/**
+ * create Invoices
+ * @openapi
+ * /api/invoices/{employeeId}:
+ *   post:
+ *     tags:
+ *      - Invoices
+ *     description: API for creating new Invoices
+ *     summary: Customer Invoices
+ *     parameters:
+ *       - name: employeeId
+ *         in: path
+ *         required: true
+ *         description: Employee Id
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Invoice Information
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - customerEmail
+ *               - phoneNumber
+ *               - fullName
+ *               - lineItems
+ *               - partsAmount
+ *               - laborAmount
+ *               - lineItemTotal
+ *               - invoiceTotal
+ *             properties:
+ *               customerEmail:
+ *                 type: string
+ *               phoneNumber:
+ *                 type: number
+ *               fullName:
+ *                 type: string
+ *               lineItems:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     itemId:
+ *                       type: number
+ *                     itemName:
+ *                       type: string
+ *                     price:
+ *                       type: number
+ *               partsAmount:
+ *                 type: number
+ *               laborAmount:
+ *                 type: number
+ *               lineItemTotal:
+ *                 type: number
+ *               invoiceTotal:
+ *                 type: number
+ *     responses:
+ *       '201':
+ *         description: Invoice created
+ *       '400':
+ *         description: Bad request
+ *       '404':
+ *         description: Employee not found
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.post('/:employeeId', (req, res, next) => {
   try {
+    // Get the employee ID from the request parameters
+    let employeeId = req.params.employeeId;
+
+    // ParseInt the employee ID
+    employeeId = parseInt(employeeId, 10);
+
+    // If employeeId is NaN; then return status code 400 with message 'Bad request'
+    if(isNaN(employeeId)) {
+      console.error('Employee Id needs to be a number:', employeeId);
+      return next(createError(400, `Bad request: ${employeeId}`));
+    }
+
     // Get the data from the request body
-    const { employeeId, customerEmail, phoneNumber, fullName, lineItems, partsAmount, laborAmount, lineItemTotal, invoiceTotal } = req.body;
+    const invoice = req.body;
 
     // Parse the number variables
-    phoneNumber = parseInt(phoneNumber, 10);
-    partsAmount = parseInt(partsAmount, 10);
-    laborAmount = parseInt(laborAmount, 10);
-    lineItemTotal = parseInt(lineItemTotal, 10);
-    invoiceTotal = parseInt(invoiceTotal, 10);
+    invoice.phoneNumber = parseInt(invoice.phoneNumber, 10);
+    invoice.partsAmount = parseFloat(invoice.partsAmount, 10);
+    invoice.laborAmount = parseFloat(invoice.laborAmount, 10);
+    invoice.lineItemTotal = parseFloat(invoice.lineItemTotal, 10);
+    invoice.invoiceTotal = parseFloat(invoice.invoiceTotal, 10);
 
     // Check if the phoneNumber, partsAmount, laborAmount, lineItemTotal and/or invoiceTotal are numbers; if not then return status code 400 with message 'Bad request'
-    if(isNaN(phoneNumber) || isNaN(partsAmount) || isNaN(laborAmount) || isNaN(lineItemTotal) || isNaN(invoiceTotal)) {
+    if(isNaN(invoice.phoneNumber)
+    || isNaN(invoice.partsAmount)
+    || isNaN(invoice.laborAmount)
+    || isNaN(invoice.lineItemTotal)
+    || isNaN(invoice.invoiceTotal)) {
       console.error('Make sure phoneNumber, partsAmount, laborAmount, lineItemTotal and/or invoiceTotal and all number type.');
       return next(createError(400, 'Bad request: Inputs must be numbers.'));
     }
 
-    // Validate the lineItems against the schema
-    const validate = ajvInstance.compile(lineItemSchema);
-    const valid = validate(lineItems);
+    // Validate the invoice against the schema
+    const validate = ajvInstance.compile(invoiceSchema);
+    const valid = validate(invoice);
 
-    // If the lineItems object doesn't match the lineItemSchema; then return status code 400 with message 'Bad request'
+    // If the invoice object doesn't match the invoiceSchema; then return status code 400 with message 'Bad request'
     if(!valid) {
-      console.error('Error validating lineItems object against the schema', validate.errors);
+      console.error('Error validating invoice object against the schema', validate.errors);
       return next(createError(400, `Bad request: ${validate.errors}`));
     }
 
@@ -102,18 +187,18 @@ router.post('/invoices/:employeeId', (req, res, next) => {
       const newInvoiceId = lastInvoice.invoiceId + 1;
       console.log(`New invoice id: ${newInvoiceId}`);
 
-      // Create the new invoice object
+      // Create the new invoice object.
       const newInvoice = {
         invoiceId: newInvoiceId,
         employeeId: employeeId,
-        customerEmail: customerEmail,
-        phoneNumber: phoneNumber,
-        fullName: fullName,
-        lineItems: lineItems,
-        partsAmount: partsAmount,
-        laborAmount: laborAmount,
-        lineItemTotal: lineItemTotal,
-        invoiceTotal: invoiceTotal,
+        customerEmail: invoice.customerEmail,
+        phoneNumber: invoice.phoneNumber,
+        fullName: invoice.fullName,
+        lineItems: invoice.lineItems,
+        partsAmount: invoice.partsAmount,
+        laborAmount: invoice.laborAmount,
+        lineItemTotal: invoice.lineItemTotal,
+        invoiceTotal: invoice.invoiceTotal,
         orderDate: moment().tz('America/Chicago').format('dddd MM-DD-YYYY') // CST
       };
 
@@ -130,19 +215,19 @@ router.post('/invoices/:employeeId', (req, res, next) => {
 });
 
 
-router.put('/profile/:employeeId', (req, res, next) =>{ 
+router.put('/profile/:employeeId', (req, res, next) =>{
   try {
     //find all invoices
 
     //get result projections (ID's and quanity of each item)
 
     //go through all invoices and check for itemId and quantity
-    
-    //add total for each line item and save results for each in array
-    
-    //send results 
 
-  // Mongo DB error handling 
+    //add total for each line item and save results for each in array
+
+    //send results
+
+  // Mongo DB error handling
   } catch (err) {
     console.error("Database Error:", err);
     next(err);
