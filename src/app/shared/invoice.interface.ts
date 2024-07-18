65a33d9c52b5e576be3e32dfc75e583fb03755e5
Zadkiel <zadkiel679@gmail.com>
2024-07-18 12:41:16 -0400

    Started the service graph setup.

---
+++ src/app/shared/invoice.interface.ts
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title: invoice.interface.ts
+; Author: Professor Krasso
+; Date: 02. July, 2024
+; Modified by: Joanna Brumfield and Zadkiel Rodriguez Alvarado
+; Description: Invoice interface
+;===========================================
+*/
+
+interface lineItem {
+    itemId: number;
+    itemName: string;
+    price: number;
+}
+
+export interface Invoice {
+    invoiceId: number;
+    employeeId: number;
+    customerEmail: string;
+    phoneNumber: number;
+    fullName: string;
+    lineItems: Array<lineItem>
+    partsAmount?: number; // Maybe provided
+    laborAmount?: number; // Maybe provided
+    lineItemTotal: number;
+    invoiceTotal: number;
+    orderDate: string;
+}
---
+++ src/app/shared/invoice.service.ts
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title: invoice.service.ts
+; Author: Professor Krasso
+; Date: 02. July, 2024
+; Modified by: Joanna Brumfield and Zadkiel Rodriguez Alvarado
+; Description: Invoice service
+;===========================================
+*/
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Invoice } from './invoice.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+
+  constructor(private http: HttpClient) { }
+
+  createInvoice(invoice: Invoice) {
+    this.http.post('/api/invoices/' + invoice.employeeId, invoice);
+  }
+
+  findAllServiceRequests() {
+    return this.http.get('/api/invoices/service-graph');
+  }
+}
