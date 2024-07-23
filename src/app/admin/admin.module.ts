ee8c03525cc1f08a239ba919c69a35f9cdbc3582
Zadkiel <zadkiel679@gmail.com>
2024-07-23 17:32:25 -0400

    Refactor the service graph component.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,8 +10,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
+import { ChartModule } from 'primeng/chart';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
@@ -21,8 +22,6 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { ServiceGraphComponent } from './service-graph/service-graph.component';
-import { NavComponent } from '../shared/layouts/nav/nav.component';
-import { FooterComponent } from '../shared/layouts/footer/footer.component';
 
 
 @NgModule({
@@ -32,8 +31,6 @@ import { FooterComponent } from '../shared/layouts/footer/footer.component';
     EditEmployeeComponent,
     CreateEmployeeComponent,
     ServiceGraphComponent,
-    // NavComponent,
-    // FooterComponent
   ],
   imports: [
     CommonModule,
@@ -41,7 +38,8 @@ import { FooterComponent } from '../shared/layouts/footer/footer.component';
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    ChartModule
   ],
   exports: [
     AdminComponent,
--- src/app/admin/service-graph/service-graph.component.ts
+++ src/app/admin/service-graph/service-graph.component.ts
@@ -7,79 +7,49 @@
 ; Description: Service Graph Component
 ;===========================================
 */
-import { Component, OnInit, OnChanges, SimpleChanges, AfterViewInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from 'src/app/shared/invoice.service';
-import { Chart, registerables, ChartConfiguration } from 'chart.js';
-Chart.register(...registerables);
 
 @Component({
   selector: 'app-service-graph',
   templateUrl: './service-graph.component.html',
   styleUrls: ['./service-graph.component.scss']
 })
-export class ServiceGraphComponent implements OnInit, OnChanges, AfterViewInit {
-  // local variables
+export class ServiceGraphComponent implements OnInit {
+  // Chart variables
   pieChartCheck: boolean = true;
   barChartCheck: boolean = false;
-  pieChart: Chart<'pie', number[], string> | null = null;
-  barChart: Chart<'bar', { x: string; y: number }[]> | null = null;
+  pieChartData: any;
+  barChartData: any;
 
-  servicesData: any[] = [];
-
-  // ViewChild decorator to reference the canvas element in the html
-  @ViewChild('myPieChart') myPieChart!: ElementRef<HTMLCanvasElement>;
-  @ViewChild('myStackedChart') myBarChart!: ElementRef<HTMLCanvasElement>;
-
-  constructor(private cdr: ChangeDetectorRef, private invoiceService: InvoiceService) {}
+  constructor(private invoiceService: InvoiceService) {}
 
   ngOnInit(): void {
-    // Get the services data
+    // Get all the services
     this.invoiceService.findAllServiceRequests().subscribe({
       next: (data: any) => {
-        this.servicesData = data;
-        this.cdr.detectChanges();
-        if (this.pieChartCheck) {
-          this.renderPieChart();
-        } else if (this.barChartCheck) {
-          this.renderStackedChart();
-        }
+        // Process the data received
+        this.processChartData(data);
       },
       error: (err) => {
+        // Console error if there was an error fetching the data
         console.error('Error getting the data: ', err);
       }
-    })
+    });
   }
 
-  ngAfterViewInit(): void {}
+  processChartData(data: any): void {
+    // Map the labels from the item title
+    const labels = data.map((item: any) => item._id.title);
+    // Map the count of services from the item counts
+    const itemCount = data.map((item: any) => item.itemCount);
 
-  ngOnChanges(changes: SimpleChanges): void {
-    // Check if the pieChartCheck has changed and is now true to re-render the pie chart
-    if (changes['pieChartCheck'] && this.pieChartCheck) {
-      this.cdr.detectChanges();
-      this.renderPieChart();
-    }
-  }
-
-  renderPieChart(): void {
-    // Get the pie chart canvas element
-    const ctx = this.myPieChart.nativeElement;
-
-    // Destroy existing chart instance if it exists to prevent duplicates
-    if (this.pieChart) {
-      this.pieChart.destroy();
-    }
-
-    // Map the serviceData retrieved to the labels and data of the pie chart
-    const labels = this.servicesData.map(item => item._id.title);
-    const data = this.servicesData.map(item => item.itemCount);
-
-    // Pie Chart Configuration
-    const config: ChartConfiguration<'pie', number[], string> = {
-      type: 'pie',
-      data: {
-        labels: labels,
-        datasets: [{
-          data: data,
+    // Construct the pie chart
+    this.pieChartData = {
+      labels: labels,
+      datasets: [
+        {
+          data: itemCount,
           backgroundColor: [
             '#F8A1A1',
             '#FFB77D',
@@ -98,87 +68,29 @@ export class ServiceGraphComponent implements OnInit, OnChanges, AfterViewInit {
             '#AF593E',
             '#6CDAE7'
           ]
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false
-      }
-    };
-    // Create the new Chart
-    this.pieChart = new Chart<'pie', number[], string>(ctx, config);
-  }
-
-  renderStackedChart() {
-    // Get the stacked chart canvas element
-    const ctx = this.myBarChart.nativeElement;
-
-    // Destroy existing chart instance if it exists to prevent duplicates
-    if(this.barChart) {
-      this.barChart.destroy();
-    }
-
-    // Map the serviceData retrieved to the labels and data of the pie chart
-    const labels = this.servicesData.map(item => item._id.title);
-    const data = this.servicesData.map(item => ({ x: item._id.title, y: item.itemCount }));
-
-    // Bar Chart Configuration
-    const config: ChartConfiguration<'bar', { x: string; y: number }[], string> = {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Service Counts',
-          data: data,
-          backgroundColor: '#22AF31',
-          hoverBackgroundColor: '#224D31',
-          barPercentage: 0.5,
-          categoryPercentage: 0.5
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            stacked: true
-          },
-          y: {
-            stacked: true
-          }
         }
-      }
+      ]
     };
-    // Create the new Chart
-    this.barChart = new Chart<'bar', { x: string; y: number }[]>(ctx, config);
+
+    // Construct the bar chart
+    this.barChartData = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Service Counts',
+          data: itemCount,
+          backgroundColor: '#22AF31',
+          hoverBackgroundColor: '#224D31'
+        }
+      ]
+    };
   }
 
+  // Function to switch between the pie and bar charts
   changeChartView(): void {
-    // Change the booleans value to the opposite
     this.pieChartCheck = !this.pieChartCheck;
     this.barChartCheck = !this.barChartCheck;
-
-    // If the pieChartCheck is true; then detect changes in the DOM and render the pie chart
-    if (this.pieChartCheck) {
-      this.cdr.detectChanges();
-      this.renderPieChart();
-
-      // Else if the barChartCheck is true; then detect changes in the DOM and render the bar chart
-    } else if (this.barChartCheck) {
-      this.cdr.detectChanges();
-      this.renderStackedChart();
-    } else {
-      // If there is already a pie chart that exist; then destroy it
-      if (this.pieChart) {
-        this.pieChart.destroy();
-        this.pieChart = null;
-      }
-      // If there is already a bar chart that exist; then destroy it
-      if (this.barChart) {
-        this.barChart.destroy();
-        this.barChart = null;
-      }
-    }
   }
 }
+
 
